11f60cae89560b37f073f3bb2d712914fc5a0750
Steve <stevetrease@gmail.com>
2018-11-25 21:54:09 +0300

    First build.

---
+++ Image Blocker blocker/ContentBlockerRequestHandler.swift
@@ -0,0 +1,23 @@
+//
+//  ContentBlockerRequestHandler.swift
+//  Image Blocker blocker
+//
+//  Created by Steve on 25/11/2018.
+//  Copyright © 2018 Steve. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ContentBlockerRequestHandler: NSObject, NSExtensionRequestHandling {
+
+    func beginRequest(with context: NSExtensionContext) {
+        let attachment = NSItemProvider(contentsOf: Bundle.main.url(forResource: "blockerList", withExtension: "json"))!
+        
+        let item = NSExtensionItem()
+        item.attachments = [attachment]
+        
+        context.completeRequest(returningItems: [item], completionHandler: nil)
+    }
+    
+}
