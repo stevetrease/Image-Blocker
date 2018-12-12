b63f5f1280f934b091748b77bccf54d6889a0977
Steve <stevetrease@gmail.com>
2018-12-12 19:39:54 +0300

    Display status on screen.

--- Image Blocker/ViewController.swift
+++ Image Blocker/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import SafariServices
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blockerStatus: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print (NSURL (fileURLWithPath: "\(#file)").lastPathComponent!, "\(#function)")
+        update()
     }
-
-
+    
+    
+    @IBAction func screenTappedTriggered(sender: AnyObject) {
+        print (NSURL (fileURLWithPath: "\(#file)").lastPathComponent!, "\(#function)")
+        print ("tapped")
+        update()
+    }
+    
+    
+    func update () {
+        print (NSURL (fileURLWithPath: "\(#file)").lastPathComponent!, "\(#function)")
+        SFContentBlockerManager.getStateOfContentBlocker(withIdentifier: "eu.trease.Image-Blocker.Image-Blocker-blocker") { state, error in
+            DispatchQueue.main.async {
+                guard let state = state else {
+                    print("Detection error: \(error!.localizedDescription)")
+                    return
+                }
+                if (state.isEnabled) {
+                    self.blockerStatus.text = "Enabled"
+                } else {
+                    self.blockerStatus.text = "Disabled"
+                }
+            }
+        }
+    }
 }
-
